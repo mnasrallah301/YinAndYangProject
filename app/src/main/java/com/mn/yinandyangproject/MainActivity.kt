438e8a14caaf38a6ab3610a5b06c394ed1c1701a
Marwa Nasrallah <m.nasrallah@useradgents.com>
2023-09-05 17:08:26 +0100

    init

--- app/src/main/java/com/mn/yinandyangproject/MainActivity.kt
+++ app/src/main/java/com/mn/yinandyangproject/MainActivity.kt
@@ -3,6 +3,13 @@ package com.mn.yinandyangproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.mn.yinandyangproject.ui.theme.YinAndYangProjectTheme
 
 class MainActivity : ComponentActivity() {
